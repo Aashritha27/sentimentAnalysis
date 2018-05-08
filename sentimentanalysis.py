d22ae048af2ffd8034dd4c7fae680afce6a754b6
ssrivasan2011 <ssrivasan2011@gmail.com>
2018-05-08 22:16:20 +0530

    sentiment to file
    Sir you can check the Sentiment.csv file to see the output.

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -29,6 +29,7 @@ class sentimentAnalysis(object):
 		try:
 			self.Data['sentimentPercentage'] = self.Data['contents'].apply(sentiments,args=(self.logger,))
 			self.Data['sentiments'] = self.Data['sentimentPercentage].apply(sentimentReviews,args=(self.logger,))
+			self.Data.to_csv('Sentiment.csv',sep=',')
 		except Exception as e:
 			self.logger.error('cannot define sentiment {}'.format(e),exc_info=True)
 		
